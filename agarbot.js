3738fd64e0b5af5cf7d79f6bd55a251f6e110e48
Neonx99 <48287047+Neonx99@users.noreply.github.com>
2019-07-18 09:24:14 +0200

    Update agarbot.js

--- agarbot.js
+++ agarbot.js
@@ -8,17 +8,19 @@ let embed = new RichEmbed();
 let prefix = config.prefix;
 
 bot.on('ready', () => {
-    bot.user.setActivity(`${config.prefix}help`, { type: 'PLAYING' });
+    bot.user.setActivity(`${prefix}help`, { type: 'PLAYING' });
     font('AgarBot', 'Doom', art => {
         console.log(art);
     });
 });
+
 bot.on('message', async msg => {
     if (!msg.content.toLocaleLowerCase().startsWith(prefix)) return;
     if (msg.author.bot || msg.channel.type === 'dm') return;
     const args = msg.content.substring(prefix.length).split(' ');
 
     switch (args[0].toLocaleLowerCase()) {
+
         case 'ping':
             embed = new RichEmbed();
             embed.addField('Bot ping', Math.round(bot.ping));
@@ -28,137 +30,9 @@ bot.on('message', async msg => {
 
         case 'help':
             embed = new RichEmbed();
-            embed.addField('Commands', `${config.prefix}party\n${config.prefix}agar\n${config.prefix}invite\n${config.prefix}ping`);
+            embed.addField('Commands', `${prefix}party\n${prefix}agar\n${prefix}invite\n${prefix}ping`);
             embed.setColor('RANDOM');
             msg.channel.send(embed);
-            break;
-
-        case 'party':
-            if (!args[1]) {
-                embed = new RichEmbed();
-                embed.setColor('RANDOM');
-                embed.addField('Agar.io Regions', 'CN (China)\nTK (Turkey)\nEU (London)\nUS (Atlanta)\nBR (Brazil)\nJP (Tokyo)\nRU (Russia)\nSG (Singapore)');
-                embed.addField('Creating party', `${config.prefix}party region`);
-                embed.addField('Checking party', `${config.prefix}party code`);
-                return msg.channel.send(embed);
-            }
-            if (args[1].length == 2) {
-                switch (args[1].toUpperCase()) {
-                    case 'EU':
-                        var rtoken = [10, 19, 10, 15, 69, 85, 45, 76, 111, 110, 100, 111, 110, 58, 112, 97, 114, 116, 121, 18, 0];
-                        var bytes = [10, 19, 10, 9, 69, 85, 45, 76, 111, 110, 100, 111, 110, 18, 6, 58, 112, 97, 114, 116, 121];
-                        var region = 'EU-London';
-                        break;
-                    case 'RU':
-                        rtoken = [10, 19, 10, 15, 82, 85, 45, 82, 117, 115, 115, 105, 97, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = [10, 19, 10, 9, 82, 85, 45, 82, 117, 115, 115, 105, 97, 18, 6, 58, 112, 97, 114, 116, 121];
-                        region = 'RU-Russia';
-                        break;
-                    case 'TK':
-                        rtoken = [10, 19, 10, 15, 84, 75, 45, 84, 117, 114, 107, 101, 121, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = [10, 19, 10, 9, 84, 75, 45, 84, 117, 114, 107, 101, 121, 18, 6, 58, 112, 97, 114, 116, 12];
-                        region = 'TK-Turkey';
-                        break;
-                    case 'CN':
-                        rtoken = [10, 18, 10, 14, 67, 78, 45, 67, 104, 105, 110, 97, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = rtoken;
-                        region = 'CN-China';
-                        break;
-                    case 'US':
-                        rtoken = [10, 20, 10, 16, 85, 83, 45, 65, 116, 108, 97, 110, 116, 97, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = rtoken;
-                        region = 'US-Atlanta';
-                        break;
-                    case 'JP':
-                        rtoken = [10, 18, 10, 14, 74, 80, 45, 84, 111, 107, 121, 111, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = rtoken;
-                        region = 'JP-Tokyo';
-                        break;
-                    case 'BR':
-                        rtoken = [10, 19, 10, 15, 66, 82, 45, 66, 114, 97, 122, 105, 108, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = rtoken;
-                        region = 'BR-Brazil';
-                        break;
-                    case 'SG':
-                        rtoken = [10, 22, 10, 18, 83, 71, 45, 83, 105, 110, 103, 97, 112, 111, 114, 101, 58, 112, 97, 114, 116, 121, 18, 0];
-                        bytes = rtoken;
-                        region = 'SG-Singapore';
-                        break;
-                    default:
-                        embed = new RichEmbed();
-                        embed.setColor('RANDOM');
-                        embed.addField('Agar.io Regions', 'CN (China)\nTK (Turkey)\nEU (Europe)\nUS (Atlanta)\nBR (Brazil)\nJP (Tokyo)\nRU (Russia)\nSG (Singapore)');
-                        embed.addField('Creating party', `${config.prefix} region`);
-                        embed.addField('Checking party', `${config.prefix}party code`);
-                        msg.channel.send(embed);
-                        break;
-                }
-                let xclientkey = await request('https://agar.io/mc/agario.js');
-                let versionString = xclientkey.match(/(?<=versionString=")[^"]+/)[0];
-                let versionInt = parseInt(versionString.split(".")[0]) * 10000 + parseInt(versionString.split(".")[1]) * 100 + parseInt(versionString.split(".")[2]);
-
-                request({
-                    url: 'https://webbouncer-live-v7-0.agario.miniclippt.com/v4/findServer',
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json', 'x-client-version': versionInt },
-                    body: Buffer.from(bytes)
-                }).then(body => {
-                    body = JSON.parse(body);
-                    embed = new RichEmbed();
-                    embed.setColor('RANDOM');
-                    if (body.status === 'no_servers') {
-                        request({
-                            url: 'https://webbouncer-live-v7-0.agario.miniclippt.com/v4/createToken',
-                            method: 'POST',
-                            headers: { 'Content-Type': 'application/json', 'x-client-version': versionInt },
-                            body: Buffer.from(rtoken)
-                        }).then(body => {
-                            body = JSON.parse(body);
-                            let partyToken = body.token;
-                            let bytez = [10, 17, 10, 9, 69, 85, 45, 76, 111, 110, 100, 111, 110, 18, 4, 58, 102, 102, 97, 26, 8, 10, 6];
-                            for (let i = 0; i < partyToken.length; i++) bytez.push(partyToken.charCodeAt(i));
-                            request({
-                                url: 'https://webbouncer-live-v7-0.agario.miniclippt.com/v4/getToken',
-                                method: 'POST',
-                                headers: { 'Content-Type': 'application/json', 'x-client-version': versionInt },
-                                body: Buffer.from(bytez)
-                            }).then(body => {
-                                body = JSON.parse(body);
-                                embed = new RichEmbed();
-                                embed.setColor('RANDOM');
-                                embed.addField('Party info', `Link: https://agar.io/#${partyToken}\nRegion: ${region}\nCode: ${partyToken}\nIP: ${body.endpoints.https.includes('ip') ? 'ws://' + body.endpoints.https : 'wss://' + body.endpoints.https}?party_id=${partyToken}`);
-                                msg.channel.send(embed);
-                            });
-                        });
-                    }
-                    else {
-                        embed.addField('Party info', `Link: https://agar.io/#${body.token}\nRegion: ${region}\nCode: ${body.token}\nIP: ${body.endpoints.https.includes('ip') ? 'ws://' + body.endpoints.https : 'wss://' + body.endpoints.https}?party_id=${body.token}`);
-                        msg.channel.send(embed);
-                    }
-                });
-            }
-            else {
-                if (args[1].startsWith('#')) args[1] = args[1].split('#')[1];
-                let bytez = [10, 17, 10, 9, 69, 85, 45, 76, 111, 110, 100, 111, 110, 18, 4, 58, 102, 102, 97, 26, 8, 10, 6];
-                for (let i = 0; i < args[1].length; i++) bytez.push(args[1].toUpperCase().charCodeAt(i));
-                request({
-                    url: 'https://webbouncer-live-v7-0.agario.miniclippt.com/v4/getToken',
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json', 'x-client-version': versionInt },
-                    body: Buffer.from(bytez)
-                }).then(body => {
-                    body = JSON.parse(body);
-                    embed = new RichEmbed();
-                    embed.setColor('RANDOM');
-                    embed.addField('Party info', `IP: ${body.endpoints.https.includes('ip') ? 'ws://' + body.endpoints.https : 'wss://' + body.endpoints.https}?party_id=${args[1].toUpperCase()}\nStatus: ${body.status}`);
-                    msg.channel.send(embed);
-                }).catch(() => {
-                    embed = new RichEmbed();
-                    embed.setColor('RANDOM');
-                    embed.setTitle('invalid party code');
-                    msg.channel.send(embed);
-                });
-            }
             break;
 
         case 'invite':
@@ -171,12 +45,12 @@ bot.on('message', async msg => {
 
         case 'agar':
             let webBouncer = await request('https://webbouncer-live-v7-0.agario.miniclippt.com/info');
-            let xclientkey = await request('https://agar.io/mc/agario.js');
-            let versionString = xclientkey.match(/(?<=versionString=")[^"]+/)[0];
-            let versionInt = parseInt(versionString.split('.')[0]) * 10000 + parseInt(versionString.split('.')[1]) * 100 + parseInt(versionString.split('.')[2]);
+            var xclientkey = await request('https://agar.io/mc/agario.js');
+            var versionString = xclientkey.match(/(?<=versionString=")[^"]+/)[0];
+            var versionInt = parseInt(versionString.split('.')[0]) * 10000 + parseInt(versionString.split('.')[1]) * 100 + parseInt(versionString.split('.')[2]);
             let init = new Uint8Array(new Uint32Array([versionInt]).buffer);
             let core = await request('https://agar.io/agario.core.js');
-            let protocolVersion = core.match(/d;..\(f,(\d+)\);/)[1]
+            let protocolVersion = core.match(/d;..\(f,(\d+)\);/)[1];
             let agar = JSON.parse(webBouncer);
             embed = new RichEmbed();
             embed.setColor('RANDOM');
@@ -185,8 +59,141 @@ bot.on('message', async msg => {
             embed.addField('Info', `Protocol Key: ${versionInt}\nProtocol Version: ${protocolVersion}\n[ ${[255, init]} ]`);
             msg.channel.send(embed);
             break;
+
+        case 'party':
+            if (!args[1] || args[1].length == 2 && !args[1].toUpperCase().match(/EU|RU|TK|CN|US|JP|BR|SG/)) {
+                embed = new RichEmbed();
+                embed.setColor('RANDOM');
+                embed.addField('Agar.io Regions', 'CN (China)\nTK (Turkey)\nEU (London)\nUS (Atlanta)\nBR (Brazil)\nJP (Tokyo)\nRU (Russia)\nSG (Singapore)');
+                embed.addField('Creating party', `${prefix}party region`);
+                embed.addField('Checking party', `${prefix}party code`);
+                return msg.channel.send(embed);
+            }
+
+            xclientkey = await request('https://agar.io/mc/agario.js');
+            versionString = xclientkey.match(/(?<=versionString=")[^"]+/)[0];
+            versionInt = parseInt(versionString.split('.')[0]) * 10000 + parseInt(versionString.split('.')[1]) * 100 + parseInt(versionString.split('.')[2]);
+
+            embed = new RichEmbed();
+            embed.setColor('RANDOM');
+
+            if (args[1].length == 2) {
+
+                requestV4('https://webbouncer-live-v7-0.agario.miniclippt.com/v4/findServer', versionInt, generateBytes(args[1]), body => {
+                    if (body.status !== 'no_servers') {
+                        embed.addField('Party info', `Link: https://agar.io/#${body.token}\nRegion: ${generateBytes(args[1], true)}\nCode: ${body.token}\nIP: ${body.endpoints.https.includes('ip') ? 'ws://' + body.endpoints.https : 'wss://' + body.endpoints.https}?party_id=${body.token}`);
+                        msg.channel.send(embed);
+                    } else {
+                        requestV4('https://webbouncer-live-v7-0.agario.miniclippt.com/v4/createToken', versionInt, generateBytes(args[1], null, true), body => {
+                            requestV4('https://webbouncer-live-v7-0.agario.miniclippt.com/v4/getToken', versionInt, generateBytes(args[1], null, null, body.token), body => {
+                                embed.addField('Party info', `Link: https://agar.io/#${partyToken}\nRegion: ${region}\nCode: ${partyToken}\nIP: ${body.endpoints.https.includes('ip') ? 'ws://' + body.endpoints.https : 'wss://' + body.endpoints.https}?party_id=${partyToken}`);
+                                msg.channel.send(embed);
+                            });
+                        });
+                    }
+                });
+
+            } else {
+                if (args[1].startsWith('#')) args[1] = args[1].split('#')[1];
+                requestV4('https://webbouncer-live-v7-0.agario.miniclippt.com/v4/getToken', versionInt, generateBytes(args[1], null, null, args[1]), body => {
+
+                    if (!body.status) {
+                        embed.setTitle('invalid party code');
+                        return msg.channel.send(embed);
+                    }
+
+                    embed.addField('Party info', `IP: ${body.endpoints.https.includes('ip') ? 'ws://' + body.endpoints.https : 'wss://' + body.endpoints.https}?party_id=${args[1].toUpperCase()}\nStatus: ${body.status}`);
+                    msg.channel.send(embed);
+
+                });
+            }
+            break;
+
     }
 });
+
+function requestV4(url, version, token, callback) {
+    request({
+        url: url,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'x-client-version': version },
+        body: Buffer.from(token)
+    }).then(body => {
+        body = JSON.parse(body);
+        return callback(body);
+    }).catch((e) => {
+        callback(e);
+    });
+}
+
+function generateBytes(args, region, backup, token) {
+    if (token) {
+        let rtoken = [10, 17, 10, 9, 69, 85, 45, 76, 111, 110, 100, 111, 110, 18, 4, 58, 102, 102, 97, 26, 8, 10, 6];
+        for (let i = 0; i < token.length; i++) rtoken.push(token.charCodeAt(i));
+        return rtoken;
+    }
+    switch (args.toUpperCase()) {
+        case 'EU':
+            var server = 'EU-London';
+            if (region) return server;
+            var bytes = [10, 19, 10, 9];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            if (backup) bytes.push(58, 112, 97, 114, 116, 121, 18, 0);
+            else bytes.push(18, 6, 58, 112, 97, 114, 116, 121);
+            return bytes;
+        case 'RU':
+            server = 'RU-Russia';
+            if (region) return server;
+            bytes = [10, 19, 10, 9];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            if (backup) bytes.push(18, 6, 58, 112, 97, 114, 116, 121);
+            else bytes.push(18, 6, 58, 112, 97, 114, 116, 121);
+            return bytes;
+        case 'TK':
+            server = 'TK-Turkey';
+            if (region) return server;
+            bytes = [10, 19, 10, 9];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            if (backup) bytes.push(18, 6, 58, 112, 97, 114, 116, 12);
+            else bytes.push(18, 6, 58, 112, 97, 114, 116, 121);
+            return bytes;
+        case 'US':
+            server = 'US-Atlanta';
+            if (region) return server;
+            bytes = [10, 20, 10, 16];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            bytes.push(58, 112, 97, 114, 116, 121, 18, 0);
+            return bytes;
+        case 'JP':
+            server = 'JP-Tokyo';
+            if (region) return server;
+            bytes = [10, 18, 10, 14];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            bytes.push(58, 112, 97, 114, 116, 121, 18, 0);
+            return bytes;
+        case 'BR':
+            server = 'BR-Brazil';
+            if (region) return server;
+            bytes = [10, 19, 10, 15];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            bytes.push(58, 112, 97, 114, 116, 121, 18, 0);
+            return bytes;
+        case 'SG':
+            server = 'SG-Singapore';
+            if (region) return server;
+            bytes = [10, 22, 10, 18];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            bytes.push(58, 112, 97, 114, 116, 121, 18, 0);
+            return bytes;
+        case 'CN':
+            server = 'CN-China';
+            if (region) return server;
+            bytes = [10, 18, 10, 14];
+            for (let i = 0; i < server.length; i++) bytes.push(server.charCodeAt(i));
+            bytes.push(58, 112, 97, 114, 116, 121, 18, 0);
+            return bytes;
+    }
+}
 
 bot.on('error', err => {
     if (err.msg === 'ECONNRESET' || err.msg === 'ERROR' || !err.msg) return;
@@ -194,5 +201,3 @@ bot.on('error', err => {
 });
 
 bot.login(config.token);
-
-process.on('uncaughtException', () => { });
